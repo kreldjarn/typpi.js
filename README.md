9cfeac4ef9f0fc08ef8d0e6175a42f6f9eb55ee0
Kristján Eldjárn <kristjan@eldjarn.net>
2014-06-26 18:02:27 +0000

    Updated README.md, added to RNG pool

---
+++ README.md
@@ -0,0 +1,4 @@
+typpi.js
+========
+
+A bare-bones node.js/socket.io chat server and client. Supports many state-of-the-art features, such as random nickname generation.
--- name_generator.js
+++ name_generator.js
@@ -57,8 +57,7 @@ prefix = ['Bjúgna',
           'Forseta',
           'Fitu',
           'Kransæða',
-          'Háls, nef og eyrna',
-          'Gin og klaufa',
+          'Háls-, nef- og eyrna',
           'Sleik',
           'Kántrý',
           'Diskó',
@@ -66,7 +65,12 @@ prefix = ['Bjúgna',
           'Frygðar',
           'Klám',
           'Tilfinninga',
-          'Getnaðar'];
+          'Getnaðar',
+          'Virðingar',
+          'Gæða',
+          'Drauga',
+          'Sykur',
+          'Eiturlyfja'];
 
 postfix = ['maður',
            'sali',
@@ -121,12 +125,32 @@ postfix = ['maður',
            'galdur',
            'pizza',
            'heilkenni',
-           'kæfa'];
+           'kæfa',
+           'klám',
+           'draugur',
+           'kisa',
+           'klístur',
+           'áróður',
+           'hvolpur',
+           'tæki',
+           'lánasjóður',
+           'moli',
+           'hrúður'];
 
-exports.random  = function()
+function generate()
 {
     return prefix[Math.floor(Math.random() * prefix.length)] + 
            postfix[Math.floor(Math.random() * postfix.length)];
+}
+
+var FORBIDDEN_NAMES = ['Barnaklám', 'blog.central.is/Prinsessa69@hotmail.com'];
+
+exports.random  = function()
+{
+    do
+        name = generate();
+    while (FORBIDDEN_NAMES.indexOf(name) >= 0)
+    return name;
 }
 
 
