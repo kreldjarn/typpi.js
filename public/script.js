9c84cdebca405b69d9518df59257a13a6c06ed09
Halldór Eldjárn <halldor@plainvanillagames.com>
2014-07-04 01:59:20 +0000

    Added a snazzy message fade-in

--- public/script.js
+++ public/script.js
@@ -31,7 +31,10 @@ function renderMessage(msg, username, date)
 	var name = $('<span class="username"></span>').text(username);
 	var timestamp = $('<span class="timestamp"></span>').text(date);
 	var body = $('<span class="message-body"></span>').append(escapeHTML(msg).autoLink());
-	message = $('<p class="message">').append(name).append(timestamp).append(body).css('color', getColor(username));
+	message = $('<p class="message hidden">').append(name).append(timestamp).append(body).css('color', getColor(username));
+
+	setTimeout(function() {chatEntries.find(".message.hidden").removeClass("hidden");}, 100);
+
 	log(message);
 	updateNotifications();
 }
